c77b35b2daa4d0e8c1f95a689b91eab745ca6697
Pablo Vinas <pablo.araujo@alu.ufc.br>
2024-11-13 18:12:24 -0300

    adicionando html nas rotas

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const path =  require("path");
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-app.get('/users/:name',(req,res)=>{
-    res.send("Usuario: " + req.params.name)
-})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
